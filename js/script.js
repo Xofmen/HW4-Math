54257ee12645a2900f1f5fee685e7a09348805ec
Xofmen <113815570+Xofmen@users.noreply.github.com>
2022-10-29 18:31:58 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.warn('Task1');
+let num_ber = prompt('напиши любое число а я скажу тебе четное ли оно')// 
+console.log(num_ber%2);
+
+console.warn('Task2');
+let x = 2
+let y = 31
+let z = 49
+let min = Math.min(x,y,z)// Узнаем минимальное значения 
+let max = Math.max(x,y,z)// Узнаем максимальное значения 
+console.log(min,max);
+
+console.warn('Task3');
+let random = Math.random()
+let string = random.toString()
+let slice = string.slice(2)
+let chislo = Number(slice)
+console.log(chislo);
+
+
+
+
+
+
+
